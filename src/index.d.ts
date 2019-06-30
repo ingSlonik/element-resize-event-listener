f4dfa8bd0dce26c5544c4b7442c17290f5fa8cfa
Filip Paulů <ing.fenix@seznam.cz>
2019-06-30 17:06:56 +0200

    Fix TypeScript declaration

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,7 @@ declare type Handler = (element: HTMLDivElement, size: Size) => void;
 
 declare type Interval = number | "animationFrame";
 
-export declare const addResizeEventListener: (element: HTMLDivElement, handler: Handler, sendActualSize: boolean = false) => void;
+export declare const addResizeEventListener: (element: HTMLDivElement, handler: Handler, sendActualSize?: boolean) => void;
 
 export declare const removeResizeEventListener: (element: HTMLDivElement, handler: Handler) => void;
 
